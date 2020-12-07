57ab5d1cd6ff5b1a7b768b2b9ce64f70a2692fb7
psbaswserver1 <psb.awsserver1@gmail.com>
2020-12-07 16:03:55 +0530

    Initial Commit

--- swpsdata/admin.py
+++ swpsdata/admin.py
@@ -4,8 +4,8 @@ from .models import tp_DeviceData, cl_DeviceData
 
 # Register your models here.
 
-admin.site.register(tp_DeviceData)
-admin.site.register(cl_DeviceData)
+@admin.register(tp_DeviceData)
+@admin.register(cl_DeviceData)
 
 
 class tp_DeviceData(ImportExportModelAdmin):
